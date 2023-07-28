6b48cf59518dd6e2dca27a19068fe9d69ffec8bd
jayw04 <37512854+jayw04@users.noreply.github.com>
2023-07-28 17:48:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,11 +47,11 @@ def get_fruit_load_list():
     
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-       my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")  
+       my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('"+new_fruit+ "')")  
        return "Thanks for adding new fruit" +new_fruit
       
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")   
-if streamlit.button('Add a fruit to the list'):
+if streamlit.button('Add Your Favorities'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
@@ -59,6 +59,12 @@ if streamlit.button('Add a fruit to the list'):
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)  
+  
+if streamlit.button('View Our Fruit list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)  
 
 #my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
