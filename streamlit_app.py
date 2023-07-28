04c4aca78de6c205f77f60f2a45f68cec88c6d90
jayw04 <37512854+jayw04@users.noreply.github.com>
2023-07-28 10:03:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 import requests
 import snowflake.connector
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My New Healthy Dinner')
 streamlit.header('Breakfast Favority')
 streamlit.text('Omega 3 & Blueberry Oatmeal ')
 streamlit.text('Kale. Spinach and Rocket Smoothie')
